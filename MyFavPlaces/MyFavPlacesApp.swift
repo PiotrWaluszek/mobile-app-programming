e68ec51f2467141abe772400266860ab50f50db9
Piotr Waluszek <waluszekp@student.agh.edu.pl>
2025-10-08 17:06:03 +0200

    Initial Commit

---
+++ MyFavPlaces/MyFavPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavPlacesApp.swift
+//  MyFavPlaces
+//
+//  Created by xdsdsx xdsdsx on 08/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyFavPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
